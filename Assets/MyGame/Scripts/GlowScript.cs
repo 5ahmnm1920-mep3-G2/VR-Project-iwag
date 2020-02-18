113f3146b3941f8919e004830f9d7ba51718baac
Tessaira <idawagenhofer9@gmail.com>
2020-02-18 15:09:47 +0100

    created tags for post prossessing, created glow material + script

---
+++ Assets/MyGame/Scripts/GlowScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowScript : MonoBehaviour
+{
+    Update()
+    {
+        if (IsTaskCompleted())
+        {
+            object.SetActive(true);
+        }
+    }
+}
