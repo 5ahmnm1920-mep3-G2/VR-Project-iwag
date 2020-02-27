1e046a56cc399bdd4896500975bb96b1fe7b479a
Tessaira <idawagenhofer9@gmail.com>
2020-02-27 22:21:27 +0100

    replaced whiteboxes with models, created textures, applied them, removed unnecessary objects such as post processing unit, created screwable tag, script now changes textures

---
+++ Assets/MyGame/Scripts/ChangeMaterialScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ChangeMaterialScript : MonoBehaviour
+{
+    public Material[] material;
+    private Renderer rend;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.tag == "screwable")
+        {
+            rend.sharedMaterial = material[1];
+        }
+    }
+}
