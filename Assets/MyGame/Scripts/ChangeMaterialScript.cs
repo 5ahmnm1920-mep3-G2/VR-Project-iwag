3334ac5e8b4bf1494a881050415f356ad6209cef
Tessaira <idawagenhofer9@gmail.com>
2020-02-28 10:48:29 +0100

    made script change globe material, delted unnecessary textures

--- Assets/MyGame/Scripts/ChangeMaterialScript.cs
+++ Assets/MyGame/Scripts/ChangeMaterialScript.cs
@@ -3,19 +3,17 @@
 public class ChangeMaterialScript : MonoBehaviour
 {
     public Material[] material;
-    private Renderer rend;
+    public MeshRenderer rend;
     
     // Start is called before the first frame update
     void Start()
     {
-        rend = GetComponent<Renderer>();
-        rend.enabled = true;
         rend.sharedMaterial = material[0];
     }
 
     void OnCollisionEnter (Collision col)
     {
-        if (col.gameObject.tag == "screwable")
+        if (col.collider.CompareTag("screwable"))
         {
             rend.sharedMaterial = material[1];
         }
